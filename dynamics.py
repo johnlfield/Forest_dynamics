ee21fbf78b31299ccdd12571d86f4e018d05bc73
johnlfield <john.l.field@gmail.com>
2015-08-21 15:39:16 -0600

    Added litter (i.e. surface fuels) and SOM pools, controlled by basic stylized turnover rate and microbial conversion efficiency parameters. Changed state variable units to carbon-only (rather than total mass) basis.  Added routine, c_plot(), to plot carbon balance by component.  Simplified landscape analysis 'spin up' routine such that every stand starts at 1915 (or an alternate pre-determined year) and all spin-up simulation results are included in plots.  Added beetle-kill events, and made fire risk a function of litter/surface fuel weight.  Updated resulting plotting routines to transform all state variable time series from lists to bumpy arrays after extraction from library to facilitate series addition.

--- dynamics.py
+++ dynamics.py
@@ -52,6 +52,8 @@ def three_PG(age, params, states):
     w_f = states['w_f'][-1]
     w_s = states['w_s'][-1]
     w_r = states['w_r'][-1]
+    w_l = states['w_l'][-1]
+    w_o = states['w_o'][-1]
 
     # calculate LAI, canopy light interception fraction
     sigma_f_conv = 0.1 * sigma_f   # specific leaf area conversion to ha/Mg
@@ -65,25 +67,56 @@ def three_PG(age, params, states):
     alpha_c = 1.8   # universal canopy quantum efficiency coefficient, gC/MJ
     f_age = age_modifier(age, age_max, n_age)
     avg_daily_C_increment = phi_pa * alpha_c * f_DT * f_age   # gC/m2/day
-    c_concentration = 0.45
-    annual_biomass_increment = (avg_daily_C_increment/c_concentration) * 365 * 0.01  # Mg/ha/y
+    # c_concentration = 0.45
+    annual_c_increment = (avg_daily_C_increment) * 365 * 0.01  # Mg/ha/y
 
-    # allocation and biomass turnover
-    # ToDo: update this with actual allometric equations
+    # calculate all turnover & transfer quantities
     # ToDo: update litterfall routine with attenuation for young stands
-    w_f += (0.33 * annual_biomass_increment) - (0.25 * w_f)
-    w_s += (0.33 * annual_biomass_increment) - (0.25 * w_s)
-    w_r += 0.33 * annual_biomass_increment
-    AGB = w_f + w_s
+    litterfall = (w_f + w_s) * 0.10
+    litter_time_constant = 4   # years
+    litter_k = (-1 * np.log(0.5)) / litter_time_constant
+    litter_turnover = w_l * (1 - np.exp(-1 * litter_k))
+    som_time_constant = 10   # years
+    som_k = (-1 * np.log(0.5)) / som_time_constant
+    som_turnover = w_o * (1 - np.exp(-1 * som_k))
+    root_turnover = 0.25 * w_r
+    microbial_efficiency = 0.25
+
+    # implement mass balance for all carbon pools
+    # ToDo: update this with actual allometric equations
+    w_f += (0.33 * annual_c_increment) - (0.25 * w_f)
+    w_s += (0.33 * annual_c_increment) - (0.25 * w_s)
+    w_r += 0.33 * annual_c_increment - root_turnover
+    w_l += litterfall - litter_turnover
+    w_o += ((root_turnover + litter_turnover) * microbial_efficiency) - som_turnover
 
     # update state variable time series
     states['age'].append(age)
     states['w_f'].append(w_f)
     states['w_s'].append(w_s)
     states['w_r'].append(w_r)
-    states['AGB'].append(AGB)
+    states['w_l'].append(w_l)
+    states['w_o'].append(w_o)
     states['LAI'].append(LAI)
     states['interception'].append(intercept_fraction)
+
+
+def c_plot(plot_object, w_l_array, w_s_array, w_f_array, w_r_array, w_o_array, time_vector, y_label):
+        w_l_plot = w_l_array
+        w_s_plot = w_l_plot + w_s_array
+        w_f_plot = w_s_plot + w_f_array
+        w_r_plot = 0 - w_r_array
+        w_o_plot = w_r_plot - w_o_array
+
+        plot_object.bar(time_vector, w_f_plot, label='Foliage', color='g', edgecolor='g')
+        plot_object.bar(time_vector, w_s_plot, label='Stems', color='y', edgecolor='y')
+        plot_object.bar(time_vector, w_l_plot, label='Litter', color='k', edgecolor='k')
+        plot_object.bar(time_vector, w_o_plot, label='SOM', color='m', edgecolor='m')
+        plot_object.bar(time_vector, w_r_plot, label='Roots', color='b', edgecolor='b')
+        plot_object.ylabel(y_label)
+        plot_object.xlabel("Time (years)")
+        plot_object.legend()
+        plot_object.show()
 
 
 # main control loop
@@ -93,7 +126,8 @@ while True:
               'w_f': [0.1],   # foliage weight, Mg/ha
               'w_s': [0.1],   # stem weight, Mg/ha
               'w_r': [0.1],   # root weight, Mg/ha
-              'AGB': [0.2],   # aboveground biomass, Mg/ha, for display purposes only
+              'w_l': [0.1],   # litter (surface biomass) weight, Mg/ha
+              'w_o': [100],   # soil organic matter weight, Mg/ha
               'LAI': [0],   # for display only
               'interception': [0]   # for display only
               }
@@ -134,87 +168,98 @@ while True:
         plt.plot(simulation_years, states['interception'])
         plt.ylabel("Canopy light\ninterception fraction")
         plt.subplot(3, 1, 3)
-        plt.plot(simulation_years, states['AGB'])
-        plt.xlabel("Time (years)")
-        plt.ylabel("Aboveground live biomass\n(Mg/ha)")
-        plt.show()
+        w_s = np.array(states['w_s'])
+        w_f = np.array(states['w_f'])
+        w_r = np.array(states['w_r'])
+        w_l = np.array(states['w_l'])
+        w_o = np.array(states['w_o'])
+        c_plot(plt, w_l, w_s, w_f, w_r, w_o, simulation_years, "Ecosystem C pools\n(MgC/ha)")
 
     elif command == 'land':
-        # determine a random distribution of starting stand ages
+        # time parameters
+        start_year = 1915
+        simulation_length = 200
+        simulation_years = range(start_year, start_year+simulation_length)
+        plot_years = range(start_year, start_year+simulation_length+1)
+
+        # initialize lists for total landscape carbon fractions
         runs = 100
-        # start_ages = np.random.normal(100, 20, runs)   # assuming median stand age corresponds to railroad logging boom
-        # print "Initial stand age distribution:"
-        # print start_ages
-        # print
-        simulation_length = 120
-        total_AGB = [0]
+        total_w_f = [0]
+        total_w_s = [0]
+        total_w_r = [0]
+        total_w_l = [0]
+        total_w_o = [0]
         for i in range(simulation_length):
-            total_AGB.append(0)
-        # run = 1
+            total_w_f.append(0)
+            total_w_s.append(0)
+            total_w_r.append(0)
+            total_w_l.append(0)
+            total_w_o.append(0)
+
+        # for each specified stand run, create & initialize a new state variable dictionary, and run through simulation
+        # years computing 3-PG model steps and adding stochastic fire, beetle, and/or harvest events where appropriate
         plt.subplot(2, 1, 1)
         for run in range(runs):
             print '\rSimulating stand %i of %i' % (run+1, runs),
-            # start_age = int(start_age)
-            start_age = 100
-            # print "Starting age:"
-            # print start_age
-            # print
-            # step through a sequence of simulation years and apply the 3-PG growth model each year
-            simulation_years = range(0, start_age+simulation_length)
+            # make a new initialized state variable dictionary
             local_states = {'age': [states['age'][0]],
                             'w_f': [states['w_f'][0]],
                             'w_s': [states['w_s'][0]],
                             'w_r': [states['w_r'][0]],
-                            'AGB': [states['AGB'][0]],
+                            'w_l': [states['w_l'][0]],
+                            'w_o': [states['w_o'][0]],
                             'LAI': [states['LAI'][0]],
                             'interception': [states['interception'][0]]
                             }
-            # print "Master copy of initial state variables:"
-            # print states
-            # print
-            # print "Local copy(?) of state variables:"
-            # print local_states
-            # print
+            # define stand starting age and stochstic variables
             age = 0
             fire_frequency = 200   # years to a stand-replacing fire
+            infest_start = 2005
+            infest_end = 2015
+            infest_risk = 0.8 / (infest_end - infest_start)
+            infested = False
             for year in simulation_years:
+                # implement infestation where appropriate.  No growth or fire occur in infestation years
                 rand = random()
-                if rand <= (1.0/fire_frequency):
-                    # print "   Fire event!"
+                if not infested and (infest_start <= year <= infest_end) and (rand <= infest_risk):
                     age = 0
                     local_states['age'].append(0)
                     local_states['w_f'].append(0.1)
                     local_states['w_s'].append(0.1)
                     local_states['w_r'].append(0.1)
-                    local_states['AGB'].append(0.2)
+                    local_states['w_l'].append(local_states['w_l'][-1]+local_states['w_f'][-1]+local_states['w_s'][-1])
+                    local_states['w_o'].append(local_states['w_o'][-1]+local_states['w_r'][-1])
                     local_states['LAI'].append(0)
                     local_states['interception'].append(0)
                 else:
-                    three_PG(age, params, local_states)
-                age += 1
-            # drop the 'spin-up' results
-            simulation_years.insert(0, 0)   # insert a value corresponding to initial conditions
-            # print "Full results:"
-            # print local_states['age']
-            for key in local_states.keys():
-                for i in range(start_age):
-                    del local_states[key][0]
-            # print
-            # print "Truncated results:"
-            # print local_states['age']
-            # print
-            # plot results
-            plt.plot(range(simulation_length+1), local_states['AGB'], color="g")
-            for i in range(len(total_AGB)):
-                total_AGB[i] += local_states['AGB'][i]
-            # run += 1
+                    # if no infestation, proceed normally with stochistic fire events or 3-PG growth step
+                    fire_risk = (1.0/fire_frequency) * (local_states['w_l'][-1]/50)
+                    rand = random()
+                    if rand <= fire_risk:
+                        age = 0
+                        local_states['age'].append(0)
+                        local_states['w_f'].append(0.1)
+                        local_states['w_s'].append(0.1)
+                        local_states['w_r'].append(0.1)
+                        local_states['w_l'].append(0.1)
+                        local_states['w_o'].append(local_states['w_o'][-1]+local_states['w_r'][-1])
+                        local_states['LAI'].append(0)
+                        local_states['interception'].append(0)
+                    else:
+                        three_PG(age, params, local_states)
+                    age += 1
+
+            AGL = np.array(local_states['w_f']) + np.array(local_states['w_s'])
+            plt.plot(plot_years, AGL, color="g")
+            total_w_f += np.array(local_states['w_f'])
+            total_w_s += np.array(local_states['w_s'])
+            total_w_r += np.array(local_states['w_r'])
+            total_w_l += np.array(local_states['w_l'])
+            total_w_o += np.array(local_states['w_o'])
 
         plt.ylabel("Aboveground live biomass\n(Mg/ha)")
         plt.subplot(2, 1, 2)
-        plt.plot(range(simulation_length+1), total_AGB)
-        plt.ylabel("Total landscape AG live\nbiomass (Mg)")
-        plt.xlabel("Time (years)")
-        plt.show()
+        c_plot(plt, total_w_l, total_w_s, total_w_f, total_w_r, total_w_o, plot_years, "Total landscape carbon (MgC)")
 
     elif command == 'q':
         print "   Quitting application..."
