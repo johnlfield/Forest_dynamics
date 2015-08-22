c42d9485add1b91b68ec3fbef994ff1eec0c6afb
johnlfield <john.l.field@gmail.com>
2015-08-21 23:47:51 -0600

    Fixed a bug where disturbance event stepwise updating was adding re-initialized biomass fractions to litter/SOM, rather than prior totals.  Added harvested infestation disturbance event (not yet implemented).  Added infestation and fire event frequency distribution plots.

--- dynamics.py
+++ dynamics.py
@@ -128,17 +128,26 @@ def unharvested_infestation(param_dictionary, state_dictionary):
     state_dictionary['w_f'].append(0.1)
     state_dictionary['w_s'].append(0.1)
     state_dictionary['w_r'].append(0.1)
-    state_dictionary['w_l'].append(state_dictionary['w_l'][-1] + state_dictionary['w_f'][-1] + state_dictionary['w_s'][-1])
-    state_dictionary['w_o'].append(state_dictionary['w_o'][-1] + (state_dictionary['w_r'][-1] * microbial_efficiency))
+    state_dictionary['w_l'].append(state_dictionary['w_l'][-1] + state_dictionary['w_f'][-2] + state_dictionary['w_s'][-2])
+    state_dictionary['w_o'].append(state_dictionary['w_o'][-1] + (state_dictionary['w_r'][-2] * microbial_efficiency))
     state_dictionary['LAI'].append(0)
     state_dictionary['interception'].append(0)
 
 
 def harvested_infestation(param_dictionary, state_dictionary):
-    """All aboveground live transferred to litter, roots transferred to SOM after microbial efficiency adjustment
+    """All aboveground live disappears (harvest counted), roots transferred to SOM after microbial efficiency adjustment
     'LAI' and 'interception' are independently calculated at every time step, so arbitrarily set to zero here
     """
-    print
+    microbial_efficiency = param_dictionary['microbial_efficiency'][0]
+    state_dictionary['age'].append(0)
+    state_dictionary['w_f'].append(0.1)
+    state_dictionary['w_s'].append(0.1)
+    state_dictionary['w_r'].append(0.1)
+    state_dictionary['w_l'].append(state_dictionary['w_l'][-1])
+    state_dictionary['w_o'].append(state_dictionary['w_o'][-1] + (state_dictionary['w_r'][-2] * microbial_efficiency))
+    state_dictionary['LAI'].append(0)
+    state_dictionary['interception'].append(0)
+    return (state_dictionary['w_f'][-1] + state_dictionary['w_s'][-1])
 
 
 def fire(param_dictionary, state_dictionary):
@@ -151,7 +160,7 @@ def fire(param_dictionary, state_dictionary):
     state_dictionary['w_s'].append(0.1)
     state_dictionary['w_r'].append(0.1)
     state_dictionary['w_l'].append(0.1)
-    state_dictionary['w_o'].append(state_dictionary['w_o'][-1] + (state_dictionary['w_r'][-1] * microbial_efficiency))
+    state_dictionary['w_o'].append(state_dictionary['w_o'][-1] + (state_dictionary['w_r'][-2] * microbial_efficiency))
     state_dictionary['LAI'].append(0)
     state_dictionary['interception'].append(0)
 
@@ -177,8 +186,11 @@ while True:
     for key in states.keys():
         print "   %s = %.3f" % (key, states[key][0])
     print
-    print "*************************************************************************************************"
-    command = raw_input("Please enter the name of the parameter or initial state value to update, 'stand' to plot stand-\nlevel results, 'land' to plot landscape-level consortium runs, or 'q' to quit:\n")
+    print "****************************************************************************"
+    command = raw_input("""Please enter the name of the parameter or initial state value to update,
+'stand' to plot stand-level results,
+'land' to plot landscape-level consortium runs, or
+'q' to quit:\n   """)
     print
 
     if command in params.keys():
@@ -272,16 +284,20 @@ while True:
         total_w_r = [0]
         total_w_l = [0]
         total_w_o = [0]
+        fires = [0]
+        infestations = [0]
         for i in range(simulation_length):
             total_w_f.append(0)
             total_w_s.append(0)
             total_w_r.append(0)
             total_w_l.append(0)
             total_w_o.append(0)
+            fires.append(0)
+            infestations.append(0)
 
         # for each specified stand run, create & initialize a new state variable dictionary, and run through simulation
         # years computing 3-PG model steps and adding stochastic fire, beetle, and/or harvest events where appropriate
-        plt.subplot(2, 1, 1)
+        plt.subplot(6, 1, 1)
         for run in range(runs):
             print '\rSimulating stand %i of %i' % (run+1, runs),
             # make a new initialized state variable dictionary
@@ -296,27 +312,31 @@ while True:
                             }
             # define stand starting age and stochstic variables
             age = 0
-            fire_frequency = 100   # years to a stand-replacing fire
+            fire_frequency = 200   # years to a stand-replacing fire
             infest_start = 2005
             infest_end = 2015
             infest_risk = 0.8 / (infest_end - infest_start)
             infested = False
+            j = 0
             for year in simulation_years:
                 # implement infestation where appropriate.  No growth or fire occur in infestation years
                 rand = random()
                 if not infested and (infest_start <= year <= infest_end) and (rand <= infest_risk):
                     age = 0
                     unharvested_infestation(params, local_states)
+                    infestations[j] += 1
                 else:
                     # if no infestation, proceed normally with stochistic fire events or 3-PG growth step
-                    fire_risk = (1.0/fire_frequency) * (local_states['w_l'][-1]/50)
+                    fire_risk = (1.0/fire_frequency) * (local_states['w_l'][-1]/20)
                     rand = random()
                     if rand <= fire_risk:
                         age = 0
                         fire(params, local_states)
+                        fires[j] += 1
                     else:
                         three_PG(age, params, local_states)
                     age += 1
+                j += 1
 
             AGL = np.array(local_states['w_f']) + np.array(local_states['w_s'])
             plt.plot(plot_years, AGL, color="g")
@@ -327,10 +347,18 @@ while True:
             total_w_l += np.array(local_states['w_l'])
             total_w_o += np.array(local_states['w_o'])
 
-        plt.ylabel("Aboveground live biomass\n(Mg/ha)")
+        plt.ylabel("AG live\n(MgC/ha)")
+        plt.subplot(6, 1, 2)
+        plt.bar(plot_years, fires)
+        plt.xlim((simulation_years[0], simulation_years[-1]))
+        plt.ylabel("Fires")
+        plt.subplot(6, 1, 3)
+        plt.bar(plot_years, infestations)
+        plt.xlim((simulation_years[0], simulation_years[-1]))
+        plt.ylabel("Infestations")
         plt.subplot(2, 1, 2)
-        c_plot(plt, total_w_l, total_w_s, total_w_f, total_w_r, total_w_o, plot_years, "Total landscape carbon (MgC)")
-        plt.legend(loc=3, prop={'size': 11})
+        c_plot(plt, total_w_l, total_w_s, total_w_f, total_w_r, total_w_o, plot_years, "Landscape C\n(MgC)")
+        plt.legend(loc=3, prop={'size': 10})
         plt.xlim((simulation_years[0], simulation_years[-1]))
         plt.show()
 
