a439d55affc15bf058bfc0ea5319ba5c221a0494
johnlfield <john.l.field@gmail.com>
2015-08-22 17:38:21 -0600

    Added an additional coarse surface fuels pool representing downed stems.  Set recalcitrance high, and adjusted partitioning such that this pool gets multiple times bigger in the course of a beetle infestation.

--- dynamics.py
+++ dynamics.py
@@ -56,6 +56,7 @@ def three_PG(age, params, states):
     w_s = states['w_s'][-1]
     w_r = states['w_r'][-1]
     w_l = states['w_l'][-1]
+    w_c = states['w_c'][-1]
     w_o = states['w_o'][-1]
 
     # calculate LAI, canopy light interception fraction
@@ -76,10 +77,14 @@ def three_PG(age, params, states):
 
     # calculate all turnover & transfer quantities
     # ToDo: update litterfall routine with attenuation for young stands
-    litterfall = (w_f + w_s) * 0.10
-    litter_time_constant = 4   # years
+    litterfall = w_f * 0.20
+    litter_time_constant = 2   # years
     litter_k = (-1 * np.log(0.5)) / litter_time_constant
     litter_turnover = w_l * (1 - np.exp(-1 * litter_k))
+    branchfall = w_s * 0.10
+    coarse_fuel_time_constant = 20   # years
+    coarse_k = (-1 * np.log(0.5)) / coarse_fuel_time_constant
+    coarse_turnover = w_c * (1 - np.exp(-1 * coarse_k))
     som_time_constant = 10   # years
     som_k = (-1 * np.log(0.5)) / som_time_constant
     som_turnover = w_o * (1 - np.exp(-1 * som_k))
@@ -87,11 +92,12 @@ def three_PG(age, params, states):
 
     # implement mass balance for all carbon pools
     # ToDo: update this with actual allometric equations
-    w_f += (0.33 * annual_c_increment) - (0.25 * w_f)
-    w_s += (0.33 * annual_c_increment) - (0.25 * w_s)
+    w_f += (0.33 * annual_c_increment) - litterfall
+    w_s += (0.33 * annual_c_increment) - branchfall   # - (0.25 * w_s)
     w_r += 0.33 * annual_c_increment - root_turnover
-    w_l += litterfall - litter_turnover
-    w_o += ((root_turnover + litter_turnover) * microbial_efficiency) - som_turnover
+    w_l += litterfall + (0.9 * branchfall) - litter_turnover
+    w_c += (0.1 * branchfall) - coarse_turnover
+    w_o += ((root_turnover + litter_turnover + coarse_turnover) * microbial_efficiency) - som_turnover
 
     # update state variable time series
     states['age'].append(age)
@@ -99,13 +105,15 @@ def three_PG(age, params, states):
     states['w_s'].append(w_s)
     states['w_r'].append(w_r)
     states['w_l'].append(w_l)
+    states['w_c'].append(w_c)
     states['w_o'].append(w_o)
     states['LAI'].append(LAI)
     states['interception'].append(intercept_fraction)
 
 
-def c_plot(plot_object, w_l_array, w_s_array, w_f_array, w_r_array, w_o_array, time_vector, y_label):
-        w_l_plot = w_l_array
+def c_plot(plot_object, w_c_array, w_l_array, w_s_array, w_f_array, w_r_array, w_o_array, time_vector, y_label):
+        w_c_plot = w_c_array
+        w_l_plot = w_c_plot + w_l_array
         w_s_plot = w_l_plot + w_s_array
         w_f_plot = w_s_plot + w_f_array
         w_r_plot = 0 - w_r_array
@@ -114,13 +122,15 @@ def c_plot(plot_object, w_l_array, w_s_array, w_f_array, w_r_array, w_o_array, t
         plot_object.bar(time_vector, w_f_plot, label='Foliage', color='g', edgecolor='g')
         plot_object.bar(time_vector, w_s_plot, label='Stems', color='y', edgecolor='y')
         plot_object.bar(time_vector, w_l_plot, label='Litter', color='k', edgecolor='k')
+        plot_object.bar(time_vector, w_c_plot, label='Coarse', color='r', edgecolor='r')
         plot_object.bar(time_vector, w_o_plot, label='SOM', color='m', edgecolor='m')
         plot_object.bar(time_vector, w_r_plot, label='Roots', color='b', edgecolor='b')
         plot_object.legend(loc=9, bbox_to_anchor=(1.0, 1.0), prop={'size': 10})
 
 
 def unharvested_infestation(param_dictionary, state_dictionary):
-    """All aboveground live transferred to litter, roots transferred to SOM after microbial efficiency adjustment
+    """All foliage transferred to litter, all stems transferred to coarse fuels,
+    roots transferred to SOM after microbial efficiency adjustment,
     'LAI' and 'interception' are independently calculated at every time step, so arbitrarily set to zero here
     """
     microbial_efficiency = param_dictionary['microbial_efficiency'][0]
@@ -128,14 +138,16 @@ def unharvested_infestation(param_dictionary, state_dictionary):
     state_dictionary['w_f'].append(0.1)
     state_dictionary['w_s'].append(0.1)
     state_dictionary['w_r'].append(0.1)
-    state_dictionary['w_l'].append(state_dictionary['w_l'][-1] + state_dictionary['w_f'][-2] + state_dictionary['w_s'][-2])
+    state_dictionary['w_l'].append(state_dictionary['w_l'][-1] + state_dictionary['w_f'][-2])
+    state_dictionary['w_c'].append(state_dictionary['w_c'][-1] + state_dictionary['w_s'][-2])
     state_dictionary['w_o'].append(state_dictionary['w_o'][-1] + (state_dictionary['w_r'][-2] * microbial_efficiency))
     state_dictionary['LAI'].append(0)
     state_dictionary['interception'].append(0)
 
 
 def harvested_infestation(param_dictionary, state_dictionary):
-    """All aboveground live disappears (harvest counted), roots transferred to SOM after microbial efficiency adjustment
+    """All foliage transferred to litter, all stems removed for harvest,
+    roots transferred to SOM after microbial efficiency adjustment,
     'LAI' and 'interception' are independently calculated at every time step, so arbitrarily set to zero here
     """
     microbial_efficiency = param_dictionary['microbial_efficiency'][0]
@@ -143,15 +155,16 @@ def harvested_infestation(param_dictionary, state_dictionary):
     state_dictionary['w_f'].append(0.1)
     state_dictionary['w_s'].append(0.1)
     state_dictionary['w_r'].append(0.1)
-    state_dictionary['w_l'].append(state_dictionary['w_l'][-1])
+    state_dictionary['w_l'].append(state_dictionary['w_l'][-1] + state_dictionary['w_f'][-2])
+    state_dictionary['w_c'].append(state_dictionary['w_c'][-1])
     state_dictionary['w_o'].append(state_dictionary['w_o'][-1] + (state_dictionary['w_r'][-2] * microbial_efficiency))
     state_dictionary['LAI'].append(0)
     state_dictionary['interception'].append(0)
-    return state_dictionary['w_f'][-1] + state_dictionary['w_s'][-1]
+    return state_dictionary['w_s'][-2]
 
 
 def fire(param_dictionary, state_dictionary):
-    """All aboveground live and litter disappears, roots transferred to SOM after microbial efficiency adjustment
+    """All aboveground live, litter, coarse disappear, roots transferred to SOM after microbial efficiency adjustment
     'LAI' and 'interception' are independently calculated at every time step, so arbitrarily set to zero here
     """
     microbial_efficiency = param_dictionary['microbial_efficiency'][0]
@@ -160,6 +173,7 @@ def fire(param_dictionary, state_dictionary):
     state_dictionary['w_s'].append(0.1)
     state_dictionary['w_r'].append(0.1)
     state_dictionary['w_l'].append(0.1)
+    state_dictionary['w_c'].append(0.1)
     state_dictionary['w_o'].append(state_dictionary['w_o'][-1] + (state_dictionary['w_r'][-2] * microbial_efficiency))
     state_dictionary['LAI'].append(0)
     state_dictionary['interception'].append(0)
@@ -172,7 +186,8 @@ while True:
               'w_f': [0.1],   # foliage weight, Mg/ha
               'w_s': [0.1],   # stem weight, Mg/ha
               'w_r': [0.1],   # root weight, Mg/ha
-              'w_l': [0.1],   # litter (surface biomass) weight, Mg/ha
+              'w_l': [0.1],   # litter weight, Mg/ha
+              'w_c': [0.1],   # coarse surface fuels weight, Mg/ha
               'w_o': [40],   # soil organic matter weight, Mg/ha
               'LAI': [0],   # for display only
               'interception': [0]   # for display only
@@ -212,6 +227,7 @@ while True:
                         'w_s': [states['w_s'][0]],
                         'w_r': [states['w_r'][0]],
                         'w_l': [states['w_l'][0]],
+                        'w_c': [states['w_c'][0]],
                         'w_o': [states['w_o'][0]],
                         'LAI': [states['LAI'][0]],
                         'interception': [states['interception'][0]]
@@ -233,8 +249,9 @@ while True:
         w_f = np.array(local_states['w_f'])
         w_r = np.array(local_states['w_r'])
         w_l = np.array(local_states['w_l'])
+        w_c = np.array(local_states['w_c'])
         w_o = np.array(local_states['w_o'])
-        c_plot(plt, w_l, w_s, w_f, w_r, w_o, plot_years, "C pools\n(MgC/ha)")
+        c_plot(plt, w_c, w_l, w_s, w_f, w_r, w_o, plot_years, "C pools\n(MgC/ha)")
         plt.ylabel("Ecosystem C pools\n(MgC/ha)")
         plt.xlabel("Time (years)")
         plt.xlim((0, simulation_length))
@@ -246,6 +263,7 @@ while True:
                         'w_s': [states['w_s'][0]],
                         'w_r': [states['w_r'][0]],
                         'w_l': [states['w_l'][0]],
+                        'w_c': [states['w_c'][0]],
                         'w_o': [states['w_o'][0]],
                         'LAI': [states['LAI'][0]],
                         'interception': [states['interception'][0]]
@@ -265,8 +283,9 @@ while True:
         w_f = np.array(local_states['w_f'])
         w_r = np.array(local_states['w_r'])
         w_l = np.array(local_states['w_l'])
+        w_c = np.array(local_states['w_c'])
         w_o = np.array(local_states['w_o'])
-        c_plot(plt, w_l, w_s, w_f, w_r, w_o, plot_years, "Disturbance")
+        c_plot(plt, w_c, w_l, w_s, w_f, w_r, w_o, plot_years, "Disturbance")
         plt.text(40, -50, "Fire", horizontalalignment='center', verticalalignment='center')
         plt.text(80, -50, "Beetles", horizontalalignment='center', verticalalignment='center')
         plt.xlim((0, simulation_length))
@@ -299,37 +318,45 @@ while True:
         plt.axvspan(infest_start, infest_end, color='r', alpha=0.5, lw=0)
         ax5.set_title("Difference")
 
+        # define loops for simulating unharvested and harvested beetle infestation
+        descrip = ['unharvested scenario', 'harvest scenario']
         axis_objs = [[ax1, ax2], [ax3, ax4]]
         landscape_totals = []
         max_fire_freq = []
+        print 'Executing simulations.'
         for i in (0, 1):
             # initialize lists for total landscape carbon fractions
             total_w_f = [0]
             total_w_s = [0]
             total_w_r = [0]
             total_w_l = [0]
+            total_w_c = [0]
             total_w_o = [0]
             fires = [0]
             infestations = [0]
+            harvests = [0]
             for k in range(simulation_length):
                 total_w_f.append(0)
                 total_w_s.append(0)
                 total_w_r.append(0)
                 total_w_l.append(0)
+                total_w_c.append(0)
                 total_w_o.append(0)
                 fires.append(0)
                 infestations.append(0)
+                harvests.append(0)
 
             # for each specified stand run, create & initialize a new state variable dictionary, and run through simulation
             # years computing 3-PG model steps and adding stochastic fire, beetle, and/or harvest events where appropriate
             for run in range(runs):
-                print '\rSimulating stand %i of %i' % (run+1, runs),
+                print '\r   Step %i/2: simulating %s for stand %i/%i' % (i+1, descrip[i], run+1, runs),
                 # make a new initialized state variable dictionary
                 local_states = {'age': [states['age'][0]],
                                 'w_f': [states['w_f'][0]],
                                 'w_s': [states['w_s'][0]],
                                 'w_r': [states['w_r'][0]],
                                 'w_l': [states['w_l'][0]],
+                                'w_c': [states['w_c'][0]],
                                 'w_o': [states['w_o'][0]],
                                 'LAI': [states['LAI'][0]],
                                 'interception': [states['interception'][0]]
@@ -342,16 +369,18 @@ while True:
                 for year in simulation_years:
                     # implement infestation where appropriate.  No growth or fire occur in infestation years
                     rand = random()
+                    # ToDo: include a limit on minimum stand size/age for beetle infestation
                     if not infested and (infest_start <= year <= infest_end) and (rand <= infest_risk):
                         age = 0
                         if i == 0:
                             unharvested_infestation(params, local_states)
                         elif i == 1:
-                            harvested_infestation(params, local_states)
+                            harvest = harvested_infestation(params, local_states)
+                            harvests[j] -= harvest
                         infestations[j] += 1
                     else:
-                        # if no infestation, proceed normally with stochistic fire events or 3-PG growth step
-                        fire_risk = (1.0/fire_frequency) * (local_states['w_l'][-1]/20)
+                        # if no infestation, proceed normally with stochastic fire events or 3-PG growth step
+                        fire_risk = (1.0/fire_frequency) * (((local_states['w_l'][-1] * 0.1) + local_states['w_c'][-1])/20)
                         rand = random()
                         if rand <= fire_risk:
                             age = 0
@@ -365,6 +394,7 @@ while True:
                 total_w_s += np.array(local_states['w_s'])
                 total_w_r += np.array(local_states['w_r'])
                 total_w_l += np.array(local_states['w_l'])
+                total_w_c += np.array(local_states['w_c'])
                 total_w_o += np.array(local_states['w_o'])
 
             axis_objs[i][0].bar(plot_years, fires)
@@ -374,17 +404,20 @@ while True:
             axis_objs[i][0].yaxis.tick_right()
             axis_objs[i][0].set_ylabel("Fires\n\n")
 
-            c_plot(axis_objs[i][1], total_w_l, total_w_s, total_w_f, total_w_r, total_w_o, plot_years, "")
+            c_plot(axis_objs[i][1], total_w_c, total_w_l, total_w_s, total_w_f, total_w_r, total_w_o, plot_years, "")
             axis_objs[i][1].set_xlim((simulation_years[0], simulation_years[-1]))
             axis_objs[i][1].set_xticklabels([])
             axis_objs[i][1].set_ylabel("Landscape MgC")
 
-            landscape_total = total_w_f + total_w_s + total_w_r + total_w_l + total_w_o
+            landscape_total = total_w_f + total_w_s + total_w_r + total_w_l + total_w_c + total_w_o
             landscape_totals.append(landscape_total)
+            print
 
         difference = landscape_totals[1] - landscape_totals[0]
-        ax5.plot(plot_years, difference)
-        ax5.plot((simulation_years[0], simulation_years[-1]), (0, 0))
+        ax5.plot(plot_years, difference, color='r', linewidth=2.0, label='System C deficit')
+        ax5.plot(plot_years, np.cumsum(harvests), color='b', linewidth=2.0, label='Cumulative C removed')
+        ax5.legend(loc=3, prop={'size': 11})
+        ax5.plot((simulation_years[0], simulation_years[-1]), (0, 0), color='k')
         ax5.set_ylabel("Landscape MgC")
         ax5.set_xlim((simulation_years[0], simulation_years[-1]))
         ax1.yaxis.set_ticks([0, max(max_fire_freq)])
